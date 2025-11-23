e3409d72b4f26762eb16b2b12040255fe04fc20c
Muneeb Rehman <muneebbhanbhro9@gmail.com>
2025-11-23 22:54:45 +0500

    Add files via upload

---
+++ ITFA/Q4.py
@@ -0,0 +1,5 @@
+sentence = input("Enter a sentence: ")
+if "Python" in sentence:
+    print("The sentecne contains the word 'Python'.")
+elif "python" in sentence:
+    print("The sentecne contains the word 'Python'.")
